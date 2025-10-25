9cc976ad1b33cdc9a19df2fb4228374498ec95f6
Jacoder23 <Jacoder23@users.noreply.github.com>
2025-10-26 02:37:19 +0800

    fullscreen button added

---
+++ Assets/Scripts/SwitchWindowMode.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchWindowMode : MonoBehaviour
+{
+    public void ToggleFullscreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+    public void EnterFullscreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+    public void LeaveFullscreen()
+    {
+        Screen.fullScreen = false;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            EnterFullscreen();
+        }
+        else if (Input.GetKeyDown(KeyCode.F11))
+        {
+            LeaveFullscreen();
+        }
+    }
+}
